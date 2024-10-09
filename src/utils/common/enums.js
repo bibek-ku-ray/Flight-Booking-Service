b3d5e8a5c18ae729d5d39463170fda2cef470697
Bibek Ray <bi.ray1225@gmail.com>
2024-10-09 11:45:17 +0545

    Added booking model. Refactor middleware, utils, and repository files

---
+++ src/utils/common/enums.js
@@ -0,0 +1,19 @@
+const SEAT_TYPES = {
+    BUSINESS: "business",
+    ECONOMY: "economy",
+    PREMIUM_ECONOMY: "premium-economy",
+    FIRST_CLASS: "first-class",
+};
+
+const BOOKING_STATUS = {
+    BOOKED: "booked",
+    CANCELED: "canceled",
+    INITIATED: "initiated",
+    PENDING: "pending"
+};
+
+module.exports = {
+    SEAT_TYPES,
+    BOOKING_STATUS
+};
+
