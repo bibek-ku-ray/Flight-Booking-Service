37439492ebab1f4af0858177947879671f2a770e
Bibek Ray <bi.ray1225@gmail.com>
2024-10-14 19:47:18 +0545

    Added booking logic with transaction

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -5,7 +5,14 @@ class BookingRepository extends CrudRepository {
     constructor() {
         super(booking);
     }
+    async createBooking(data, transaction){
+        const response = await booking.create(data, {
+            transaction: transaction
+        })
+        return response
+    }
 }
+
 
 module.exports = {
     BookingRepository,
